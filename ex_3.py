08f385d4be00c79570d4b310bc37da5623d587de
Adam Kawałko <262329@student.pwr.edu.pl>
2022-01-17 14:31:27 +0100

    gotowy z dok

--- ex_3.py
+++ ex_3.py
@@ -1,10 +1,22 @@
 class BinHeap:
+    """
+    The lecture HEAP
+    """
     def __init__(self, max_size):
+        """
+        self.heapList: list of elements
+        self.currentSize: Size(length) of the list
+        self.max_size: maximum size of the heap
+        """
         self.heapList = [0]
         self.currentSize = 0
         self.max_size = max_size
 
     def percUp(self, i):
+        """
+        Function that moves up elements to keep order in heap
+        :param i: index od element
+        """
         while i // 2 > 0:
             if self.heapList[i] < self.heapList[i // 2]:
                 tmp = self.heapList[i // 2]
@@ -13,6 +25,10 @@ class BinHeap:
             i = i // 2
 
     def insert(self, k):
+        """
+        Function that adds element to the heap with some conditions
+        :param k: element
+        """
         while self.size() > self.max_size:
             self.delMin()
         if self.size() < self.max_size:
@@ -27,12 +43,19 @@ class BinHeap:
                 self.heapList.append(k)
                 self.currentSize = self.currentSize + 1
                 self.percUp(self.currentSize)
-        self.heapList.sort()
 
     def findMin(self):
+        """
+        Function that searches the smallest element
+        :return: The smallest element
+        """
         return self.heapList[1]
 
     def percDown(self, i):
+        """
+        Function that moves down elements to keep order in heap
+        :param i: index od element
+        """
         while (i * 2) <= self.currentSize:
             mc = self.minChild(i)
             if self.heapList[i] > self.heapList[mc]:
@@ -42,6 +65,11 @@ class BinHeap:
             i = mc
 
     def minChild(self, i):
+        """
+        Function that searches the index of the smallest child
+        :param i: the index from which we start our search
+        :return: index of the smallest child
+        """
         if i * 2 + 1 > self.currentSize:
             return i * 2
         else:
@@ -51,6 +79,10 @@ class BinHeap:
                 return i * 2 + 1
 
     def delMin(self):
+        """
+        Function that removes the smallest element and keeps heap in order
+        :return: the smallest element
+        """
         retval = self.heapList[1]
         self.heapList[1] = self.heapList[self.currentSize]
         self.currentSize = self.currentSize - 1
@@ -59,7 +91,10 @@ class BinHeap:
         return retval
 
     def buildHeap(self, alist):
-       
+        """
+        Function that creates heap from the list with some conditions
+        :param alist: list that will become a heap
+        """
         if self.size() <= self.max_size:
             i = len(alist) // 2
             self.currentSize = len(alist)
@@ -69,23 +104,33 @@ class BinHeap:
                 i = i - 1
             while self.size() > self.max_size:
                 self.delMin()
-        self.heapList.sort()
 
 
     def size(self):
+        """
+        Function that checks the size(length) of the heap
+        :return: size of the heap
+        """
         return self.currentSize
 
     def isEmpty(self):
+        """
+        Function that checks if the heap is empty
+        :return: True or False
+        """
         return self.currentSize == 0
 
     def __str__(self):
+        """
+        Function that shows the heap
+        :return: heap
+        """
         txt = "{}".format(self.heapList[1:])
         return txt
 
 
 if __name__ == '__main__':
-    bh = BinHeap(5)
-    bh.buildHeap([9, 5, 6, 2,7,20,11,4])
-    bh.insert(7)
-    bh.insert(19)
+    bh = BinHeap(2)
+    bh.buildHeap([9, 5, 6, 2])
     print(bh)
+
